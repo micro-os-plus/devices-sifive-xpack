225a7698aba84417ec48f58915f1ec953e56586b
Liviu Ionescu <ilg@livius.net>
2017-10-29 13:16:25 +0200

    include/*/fe310/defines.h: rework IOF defines

--- include/sifive-devices/fe310/defines.h
+++ include/sifive-devices/fe310/defines.h
@@ -32,23 +32,8 @@
 
 // ----------------------------------------------------------------------------
 
-// TODO: make them consistent with the naming scheme.
 #define SIFIVE_FE310_GPIO_PINS_LAST_NUMBER (31u)
 
-// IOF masks
-#define IOF0_SPI1_MASK          (0x000007FCUL)
-#define SPI11_NUM_SS     (4)
-#define IOF_SPI1_SS0          (2u)
-#define IOF_SPI1_SS1          (8u)
-#define IOF_SPI1_SS2          (9u)
-#define IOF_SPI1_SS3          (10u)
-#define IOF_SPI1_MOSI         (3u)
-#define IOF_SPI1_MISO         (4u)
-#define IOF_SPI1_SCK          (5u)
-#define IOF_SPI1_DQ0          (3u)
-#define IOF_SPI1_DQ1          (4u)
-#define IOF_SPI1_DQ2          (6u)
-#define IOF_SPI1_DQ3          (7u)
 // ----------------------------------------------------------------------------
 
 // The names of the RISCV_MMIO_ symbols are architecture specific,
@@ -56,35 +41,71 @@
 // These definitions will be used in <riscv-arch/device-functions-inlines.h>,
 // included in <micro-os-plus/device..
 
-#define IOF0_SPI2_MASK          (0xFC000000UL)
-#define SPI2_NUM_SS       (1)
-#define IOF_SPI2_SS0          (26u)
-#define IOF_SPI2_MOSI         (27u)
-#define IOF_SPI2_MISO         (28u)
-#define IOF_SPI2_SCK          (29u)
-#define IOF_SPI2_DQ0          (27u)
-#define IOF_SPI2_DQ1          (28u)
-#define IOF_SPI2_DQ2          (30u)
-#define IOF_SPI2_DQ3          (31u)
 #define RISCV_MMIO_MTIME_ADDRESS (SIFIVE_FE310_CLINT_BASE_ADDRESS + SIFIVE_FE310_CLINT_MTIME_OFFSET)
 #define RISCV_MMIO_MTIMECMP_ADDRESS (SIFIVE_FE310_CLINT_BASE_ADDRESS + SIFIVE_FE310_CLINT_MTIMECMP_OFFSET)
 
+// ----------------------------------------------------------------------------
+// Alternate IO Functions. To activate them, set the GPIO->iofen bits.
 
-//#define IOF0_I2C_MASK          (0x00003000UL)
+// IOF0. Clear the corresponding bits in GPIO->iofsel.
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_MASK            (0x000007FCul)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS0_POSITION    (2u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS0             (1u << 2u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS1_POSITION    (8u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS1             (1u << 8u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS2_POSITION    (9u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS2             (1u << 9u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS3_POSITION    (10u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SS3             (1u << 10u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_MOSI_POSITION   (3u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_MOSI            (1u << 3u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_MISO_POSITION   (4u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_MISO            (1u << 4u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SCK_POSITION    (5u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_SCK             (1u << 5u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ0_POSITION    (3u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ0             (1u << 3u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ1_POSITION    (4u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ1             (1u << 4u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ2_POSITION    (6u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ2             (1u << 6u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ3_POSITION    (7u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI1_DQ3             (1u << 7u)
 
-#define IOF0_UART0_MASK         (0x00030000UL)
-#define IOF_UART0_RX   (16u)
-#define IOF_UART0_TX   (17u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_MASK            (0xFC000000ul)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_SS0_POSITION    (26u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_SS0             (1u << 26u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_MOSI_POSITION   (27u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_MOSI            (1u << 27u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_MISO_POSITION   (28u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_MISO            (1u << 28u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_SCK_POSITION    (29u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_SCK             (1u << 29u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ0_POSITION    (27u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ0             (1u << 27u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ1_POSITION    (28u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ1             (1u << 28u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ2_POSITION    (30u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ2             (1u << 30u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ3_POSITION    (31u)
+#define SIFIVE_FE310_GPIO_IOF0_SPI2_DQ3             (1u << 31u)
 
-#define IOF0_UART1_MASK         (0x03000000UL)
-#define IOF_UART1_RX (24u)
-#define IOF_UART1_TX (25u)
+#define SIFIVE_FE310_GPIO_IOF0_UART0_MASK           (0x00030000ul)
+#define SIFIVE_FE310_GPIO_IOF0_UART0_RX_POSITION    (16u)
+#define SIFIVE_FE310_GPIO_IOF0_UART0_RX             (1u << 16u)
+#define SIFIVE_FE310_GPIO_IOF0_UART0_TX_POSITION    (17u)
+#define SIFIVE_FE310_GPIO_IOF0_UART0_TX             (1u << 17u)
 
-#define IOF1_PWM0_MASK          (0x0000000FUL)
-#define IOF1_PWM1_MASK          (0x00780000UL)
-#define IOF1_PWM2_MASK          (0x00003C00UL)
+#define SIFIVE_FE310_GPIO_IOF0_UART1_MASK           (0x03000000ul)
+#define SIFIVE_FE310_GPIO_IOF0_UART1_RX_POSITION    (24u)
+#define SIFIVE_FE310_GPIO_IOF0_UART1_RX             (1u << 24u)
+#define SIFIVE_FE310_GPIO_IOF0_UART1_TX_POSITION    (25u)
+#define SIFIVE_FE310_GPIO_IOF0_UART1_TX             (1u << 25u)
 
-// #define NUM_GPIO 32
+// IOF1. Set the corresponding bits in GPIO->iofsel.
+#define SIFIVE_FE310_GPIO_IOF1_PWM0_MASK            (0x0000000Ful)
+#define SIFIVE_FE310_GPIO_IOF1_PWM1_MASK            (0x00780000ul)
+#define SIFIVE_FE310_GPIO_IOF1_PWM2_MASK            (0x00003C00ul)
 
 // ----------------------------------------------------------------------------
 
