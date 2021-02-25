1cee5c46fec588e68f9513448ab0cd7bbea6d79d
Liviu Ionescu <ilg@livius.net>
2021-02-25 15:41:27 +0200

    cmake: simplify, remove functions

--- meta/xpack-helper.cmake
+++ meta/xpack-helper.cmake
@@ -9,40 +9,73 @@
 #
 # -----------------------------------------------------------------------------
 
+if(xpack-sifive-devices-included)
+  return()
+endif()
+
+set(xpack-sifive-devices-included TRUE)
+
 message(STATUS "Including xpack-sifive-devices...")
 
 # -----------------------------------------------------------------------------
 
-function(target_sources_xpack_sifive_devices target)
-
-  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
-
-  target_sources(
-    ${target}
-
-    PRIVATE
-      # TODO: add Arty e31/e51.
-      $<$<STREQUAL:"${xpack_device_compile_definition}","SIFIVE_FE310">:${xpack_root_folder}/src/fe310/diag/trace-uart.cpp>
-      $<$<STREQUAL:"${xpack_device_compile_definition}","SIFIVE_FE310">:${xpack_root_folder}/src/fe310/device-interrupts.cpp>
-      ${xpack_root_folder}/src/device-functions.cpp
-      ${xpack_root_folder}/src/plic-functions.cpp
-  )
-
-endfunction()
+find_package(micro-os-plus-architecture-riscv REQUIRED)
 
 # -----------------------------------------------------------------------------
 
-function(target_include_directories_xpack_sifive_devices target)
+# This also requires xpack_device_compile_definition to be defined by the user.
 
-  get_filename_component(xpack_root_folder ${CMAKE_CURRENT_FUNCTION_LIST_DIR} DIRECTORY)
+  get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
 
-  target_include_directories(
-    ${target}
+  # ---------------------------------------------------------------------------
 
-    PUBLIC
-      ${xpack_root_folder}/include
-  )
+  if(NOT TARGET xpack-sifive-devices-interface)
 
-endfunction()
+    add_library(xpack-sifive-devices-interface INTERFACE EXCLUDE_FROM_ALL)
+
+    # Hopefully the file names follow the symbol definitions.
+    string(TOLOWER ${xpack_device_compile_definition} device_name)
+
+    target_sources(
+      xpack-sifive-devices-interface
+  
+      INTERFACE
+        # TODO: add Arty e31/e51.
+        $<$<STREQUAL:"${xpack_device_compile_definition}","SIFIVE_FE310">:${xpack_current_folder}/src/fe310/diag/trace-uart.cpp>
+        $<$<STREQUAL:"${xpack_device_compile_definition}","SIFIVE_FE310">:${xpack_current_folder}/src/fe310/device-interrupts.cpp>
+        ${xpack_current_folder}/src/device-functions.cpp
+        ${xpack_current_folder}/src/plic-functions.cpp
+    )
+  
+    target_include_directories(
+      xpack-sifive-devices-interface
+  
+      INTERFACE
+        ${xpack_current_folder}/include
+    )
+
+    target_compile_definitions(
+      xpack-sifive-devices-interface
+  
+      INTERFACE
+        ${xpack_device_compile_definition}
+    )
+  
+    target_link_libraries(
+      xpack-sifive-devices-interface
+
+      INTERFACE
+        micro-os-plus::architecture-riscv
+        micro-os-plus::architecture-riscv-device
+        micro-os-plus::diag-trace-static
+    )
+
+    # -------------------------------------------------------------------------
+    # Aliases.
+
+    add_library(sifive::devices ALIAS xpack-sifive-devices-interface)
+    message(STATUS "sifive::devices")
+
+  endif()
 
 # -----------------------------------------------------------------------------
