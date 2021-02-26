8dce554e0cf29bee4783608415bf4712156b719c
Liviu Ionescu <ilg@livius.net>
2021-02-26 11:21:54 +0200

    cmake: add find_package() support

--- meta/xpack-sifive-devices-config.cmake
+++ meta/xpack-sifive-devices-config.cmake
@@ -12,11 +12,73 @@
 # https://cmake.org/cmake/help/v3.18/
 # https://cmake.org/cmake/help/v3.18/manual/cmake-packages.7.html#package-configuration-file
 
-# message(STATUS "Including sifive-devices-config...")
+if(xpack-sifive-devices-included)
+  return()
+endif()
+
+set(xpack-sifive-devices-included TRUE)
+
+message(STATUS "Including xpack-sifive-devices...")
 
 # -----------------------------------------------------------------------------
 
-include("${CMAKE_CURRENT_LIST_DIR}/xpack-helper.cmake")
+find_package(micro-os-plus-architecture-riscv REQUIRED)
 
 # -----------------------------------------------------------------------------
 
+# This also requires xpack_device_compile_definition to be defined by the user.
+
+get_filename_component(xpack_current_folder ${CMAKE_CURRENT_LIST_DIR} DIRECTORY)
+
+# -----------------------------------------------------------------------------
+
+if(NOT TARGET xpack-sifive-devices-interface)
+
+  add_library(xpack-sifive-devices-interface INTERFACE EXCLUDE_FROM_ALL)
+
+  # Hopefully the file names follow the symbol definitions.
+  string(TOLOWER ${xpack_device_compile_definition} device_name)
+
+  target_sources(
+    xpack-sifive-devices-interface
+
+    INTERFACE
+      # TODO: add Arty e31/e51.
+      $<$<STREQUAL:"${xpack_device_compile_definition}","SIFIVE_FE310">:${xpack_current_folder}/src/fe310/diag/trace-uart.cpp>
+      $<$<STREQUAL:"${xpack_device_compile_definition}","SIFIVE_FE310">:${xpack_current_folder}/src/fe310/device-interrupts.cpp>
+      ${xpack_current_folder}/src/device-functions.cpp
+      ${xpack_current_folder}/src/plic-functions.cpp
+  )
+
+  target_include_directories(
+    xpack-sifive-devices-interface
+
+    INTERFACE
+      ${xpack_current_folder}/include
+  )
+
+  target_compile_definitions(
+    xpack-sifive-devices-interface
+
+    INTERFACE
+      ${xpack_device_compile_definition}
+  )
+
+  target_link_libraries(
+    xpack-sifive-devices-interface
+
+    INTERFACE
+      micro-os-plus::architecture-riscv
+      micro-os-plus::architecture-riscv-device
+      micro-os-plus::diag-trace-static
+  )
+
+  # ---------------------------------------------------------------------------
+  # Aliases.
+
+  add_library(sifive::devices ALIAS xpack-sifive-devices-interface)
+  message(STATUS "sifive::devices")
+
+endif()
+
+# -----------------------------------------------------------------------------
